6067281f8dec728b1403af03fa2d6d5db9c5d32a
Samuel Shull <samuel.c.shull@gmail.com>
2023-01-26 07:20:01 -0600

    add styled-components, custom font

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { InputForm } from './InputForm';
 import { SubmissionsList } from './SubmissionsList';
+
+const AppWrapper = styled.div`
+    font-family: 'Roboto';
+`;
 
 export const App = () => {
     const state = useSelector((state) => state);
     console.log('State!!!', state);
     return (
-        <div>
+        <AppWrapper>
             <h1>Welcome to RCTools</h1>
             <InputForm />
             <SubmissionsList />
-        </div>
+        </AppWrapper>
     );
 };
--- react/src/components/Root.jsx
+++ react/src/components/Root.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import { App } from './App';
-
+import '../styles/fonts.css';
 
 export const Root = () => (
     <Provider store={store}>
