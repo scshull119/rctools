f29474d41407f0bba11f9d07fe0917bf5afd387a
Samuel Shull <samuel.c.shull@gmail.com>
2023-01-30 18:41:18 -0600

    combine menus and main window modules

--- fileIO.js
+++ fileIO.js
@@ -1,52 +1,14 @@
-const { dialog } = require('electron');
-
 let activeFilepath = null;
 
-async function onSaveAsMenuClick() {
-    try {
-        const { filePath, canceled } = await dialog.showSaveDialog({
-            filters: [
-                { name: 'JSON Documents', extensions: ['json'] }
-            ]
-        });
-        if (filePath && !canceled) {
-            activeFilepath = filePath;
-            console.log(`Saving file at path ${activeFilepath}`);
-        }
-    } catch(err) {
-        console.error('An error occurred displaying the Save dialog.');
-    }
+function getActiveFilepath() {
+    return activeFilepath;
 }
 
-function onSaveMenuClick() {
-    if (activeFilepath) {
-        console.log(`Saving to already open filepath: ${activeFilepath}`);
-    } else {
-        onSaveAsMenuClick();
-    }
-}
-
-async function onOpenMenuClick() {
-    try {
-        const { filePath, canceled } = await dialog.showOpenDialog({
-            properties: [
-                'openFile'
-            ],
-            filters: [
-                { name: 'JSON Documents', extensions: ['json'] }
-            ]
-        });
-        if (filePath && !canceled) {
-            activeFilepath = filePath;
-            console.log(`Opening file at path ${activeFilepath}`);
-        }
-    } catch(err) {
-        console.error(err);
-    }
+function setActiveFilepath(filepath) {
+    activeFilepath = filepath;
 }
 
 module.exports = {
-    onOpenMenuClick,
-    onSaveMenuClick,
-    onSaveAsMenuClick
+    getActiveFilepath,
+    setActiveFilepath
 };
--- mainWindow.js
+++ mainWindow.js
@@ -1,11 +1,16 @@
 const path = require('path');
-const { BrowserWindow } = require('electron');
-const { createMenus } = require('./menus');
 const {
-    onOpenMenuClick,
-    onSaveMenuClick,
-    onSaveAsMenuClick
+    app,
+    dialog,
+    BrowserWindow,
+    Menu
+} = require('electron');
+const {
+    getActiveFilepath,
+    setActiveFilepath
 } = require('./fileIO');
+
+let isOpenWindow = false;
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
@@ -15,16 +20,187 @@ function createMainWindow() {
             preload: path.join(__dirname, 'preload.js')
         }
     });
-    createMenus({
-        open: onOpenMenuClick,
-        save: onSaveMenuClick,
-        saveAs: onSaveAsMenuClick
-    });
+    isOpenWindow = true;
+    createMenus();
     mainWindow.loadFile('index.html');
 
     mainWindow.on('close', () => {
-        createMenus({ open: onOpenMenuClick });
+        isOpenWindow = false;
+        createMenus();
     });
+}
+
+function createMenus() {
+    const isMac = process.platform === 'darwin';
+
+    const template = [
+        // { role: 'appMenu' }
+        ...(isMac ? [{
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideOthers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        }] : []),
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'New',
+                    click: () => console.log('New menu item clicked'),
+                    accelerator: 'CommandOrControl+N'
+                },
+                {
+                    label: 'Open...',
+                    click: onOpenMenuClick,
+                    accelerator: 'CommandOrControl+O',
+                },
+                { type: 'separator' },
+                {
+                    label: 'Save',
+                    enabled: isOpenWindow,
+                    click: onSaveMenuClick,
+                    accelerator: 'CommandOrControl+S'
+                },
+                {
+                    label: 'Save As...',
+                    enabled: isOpenWindow,
+                    click: onSaveAsMenuClick,
+                    accelerator: 'Shift+CommandOrControl+S'
+                },
+                isMac ? { role: 'close' } : { role: 'quit' }
+            ]
+        },
+        // { role: 'editMenu' }
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                ...(isMac ? [
+                    { role: 'pasteAndMatchStyle' },
+                    { role: 'delete' },
+                    { role: 'selectAll' },
+                    { type: 'separator' },
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            { role: 'startSpeaking' },
+                            { role: 'stopSpeaking' }
+                        ]
+                    }
+                ] : [
+                    { role: 'delete' },
+                    { type: 'separator' },
+                    { role: 'selectAll' }
+                ])
+            ]
+        },
+        // { role: 'viewMenu' }
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { role: 'toggleDevTools' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac ? [
+                    { type: 'separator' },
+                    { role: 'front' },
+                    { type: 'separator' },
+                    { role: 'window' }
+                ] : [
+                    { role: 'close' }
+                ])
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click: async () => {
+                        const { shell } = require('electron');
+                        await shell.openExternal('https://electronjs.org');
+                    }
+                }
+            ]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+}
+
+async function onSaveAsMenuClick() {
+    try {
+        const { filePath, canceled } = await dialog.showSaveDialog({
+            filters: [
+                { name: 'JSON Documents', extensions: ['json'] }
+            ]
+        });
+        if (filePath && !canceled) {
+            setActiveFilepath(filePath);
+            console.log(`Saving file at path ${getActiveFilepath()}`);
+        }
+    } catch(err) {
+        console.error('An error occurred displaying the Save dialog.');
+    }
+}
+
+function onSaveMenuClick() {
+    if (getActiveFilepath()) {
+        console.log(`Saving to already open filepath: ${getActiveFilepath()}`);
+    } else {
+        onSaveAsMenuClick();
+    }
+}
+
+async function onOpenMenuClick() {
+    try {
+        const { filePath, canceled } = await dialog.showOpenDialog({
+            properties: [
+                'openFile'
+            ],
+            filters: [
+                { name: 'JSON Documents', extensions: ['json'] }
+            ]
+        });
+        if (filePath && !canceled) {
+            if (isOpenWindow) {
+                createMainWindow();
+            }
+            setActiveFilepath(filePath);
+            console.log(`Opening file at path ${getActiveFilepath()}`);
+        }
+    } catch(err) {
+        console.error(err);
+    }
 }
 
 module.exports = { createMainWindow };
