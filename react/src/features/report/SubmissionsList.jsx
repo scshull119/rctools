c72327f1164511e6db77dba7c024653153b6558f
Samuel Shull <samuel.c.shull@gmail.com>
2023-02-05 18:28:00 -0600

    use race and submission ids as list keys

--- react/src/features/report/SubmissionsList.jsx
+++ react/src/features/report/SubmissionsList.jsx
@@ -4,11 +4,11 @@ import { useSelector } from 'react-redux';
 export const SubmissionsList = () => {
     const races = useSelector((state) => state.data.races);
     return races.map((race) => (
-        <div key={race.name}>
+        <div key={race.id}>
             <h2>{race.name}</h2>
             <ul>
-                {race.submissions.map((submission, i) => (
-                    <li key={i}>{`${submission.name} (${submission.pronouns}): ${submission.finishTime}`}</li>
+                {race.submissions.map((submission) => (
+                    <li key={submission.id}>{`${submission.name} (${submission.pronouns}): ${submission.finishTime}`}</li>
                 ))}
             </ul>
         </div>
