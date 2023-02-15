88707a5fb1cf0ecf7deeec0cfc0531f76b590dbe
Samuel Shull <samuel.c.shull@gmail.com>
2023-02-14 19:58:51 -0600

    outline displays submissions in time order

--- react/src/features/report/SubmissionsList.jsx
+++ react/src/features/report/SubmissionsList.jsx
@@ -3,14 +3,18 @@ import { useSelector } from 'react-redux';
 
 export const SubmissionsList = () => {
     const races = useSelector((state) => state.data.races);
-    return races.map((race) => (
-        <div key={race.id}>
-            <h2>{race.name}</h2>
-            <ul>
-                {race.submissions.map((submission) => (
-                    <li key={submission.id}>{`${submission.name} (${submission.pronouns}): ${submission.timeString}`}</li>
-                ))}
-            </ul>
-        </div>
-    ));
+    return races.map((race) => {
+        const raceSubmissions = [...race.submissions];
+        raceSubmissions.sort((a, b) => a.timeSeconds - b.timeSeconds);
+        return (
+            <div key={race.id}>
+                <h2>{race.name}</h2>
+                <ul>
+                    {raceSubmissions.map((submission) => (
+                        <li key={submission.id}>{`${submission.name} (${submission.pronouns}): ${submission.timeString}`}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    });
 };
